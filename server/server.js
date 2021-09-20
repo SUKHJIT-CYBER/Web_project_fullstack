07975ed2067ee6fed8827ca60bd68ff5ae48782a
SUKHJIT <sukhjitminhas753159@gmail.com>
2021-09-20 23:25:27 +0530

    Setup a absic Express Js server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log("Hello Welcome ! to Jattland");
+//Import express js Frameworks
+const express = require("express");
+
+
+
+
+
+
+
+
+
+
+
+
+// Initialsingan app instant
+const app = express();
+//make app listen to port address
+const port = 5000;
+app.listen(port, () => {
+    console.log('Server started in port ${port}');
+});
