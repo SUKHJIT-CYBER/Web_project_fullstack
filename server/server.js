3fdba15635900f0a43ee7bb8a4281edb303e70ae
SUKHJIT <sukhjitminhas753159@gmail.com>
2021-09-21 16:35:52 +0530

    Added a nodemon feature

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const express = require("express");
 // Initialsingan app instant
 const app = express();
 //make app listen to port address
-const port = 5000;
+const port = 3000;
 app.listen(port, () => {
     console.log('Server started in port ${port}');
 });
