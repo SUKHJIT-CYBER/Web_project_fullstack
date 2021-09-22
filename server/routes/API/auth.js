06567ca49711e46bd79e6fa1555fa8a3bc776c32
SUKHJIT <sukhjitminhas753159@gmail.com>
2021-09-22 14:54:22 +0530

    Skeleton structure for auth endpoint

---
+++ server/routes/API/auth.js
@@ -0,0 +1,11 @@
+//import exp library
+const express = require("express");
+
+//cretae a route app
+const app = express.Router();
+
+
+
+
+//export the root
+module.
--- server/routes/api.js
+++ server/routes/api.js
@@ -5,10 +5,14 @@ const express = require("express");
 const app = express.Router();
 
 
+const auth = require("./API/auth");
+
+
 // create a get request handler
 app.get("/", (req, res) => {
     res.json('Welcome  to my Land  with API');
 });
 
+app.use("/auth ", auth);
 //export the root
 module.exports = app;
