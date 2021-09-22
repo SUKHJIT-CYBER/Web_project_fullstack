d2e6a1768b990e83656ed94aafebe807701ea634
SUKHJIT <sukhjitminhas753159@gmail.com>
2021-09-22 14:44:42 +0530

    Created a API route

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,17 +4,11 @@ const express = require("express");
 //cretae a route app
 const app = express.Router();
 
-// import the route
-const API = require("./api");
 
 // create a get request handler
 app.get("/", (req, res) => {
-    res.json('Welcome  to my Land');
+    res.json('Welcome  to my Land  with API');
 });
-
-
-// api route handler
-app.use("/api", API);
 
 //export the root
 module.exports = app;
