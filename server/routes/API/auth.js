17dba4580eb937a1c6b75a981b6f2d46e10229dd
SUKHJIT <sukhjitminhas753159@gmail.com>
2021-09-22 15:09:47 +0530

    Cread basic data structure for usrs

--- server/routes/API/auth.js
+++ server/routes/API/auth.js
@@ -5,7 +5,22 @@ const express = require("express");
 const app = express.Router();
 
 
+const users = {
+    SUKHJIT: {
+        Name: "Sukhjit singh",
+        Password: "Sukhjit",
+        personal: {
+            Email: "sukhjit@gmail.com",
+            Phone: "4864687764",
+            Country: "India",
 
+        },
+        Type: "     Admin", // student , mentor , admin ,unverified , disabled
+        Createdat: new Date(),
+        Confirmed: true
+
+    }
+}
 
 //export the root
-module.
+module.exports = app;
