6641fa4fc33cab446887c10e29e48b1107b6c6cb
SUKHJIT SINGH <78156807+SUKHJIT-CYBER@users.noreply.github.com>
2021-11-01 10:50:28 +0530

    Added Endpoint to Veify

--- server/routes/API/auth.js
+++ server/routes/API/auth.js
@@ -4,6 +4,29 @@ const express = require("express");
 //cretae a route app
 const app = express.Router();
 
+//endpont to verigy user
+app.get("/verify/:Username", (req, res) => {
+  const Username = req.params.Username;
+  const Code = req.query.Code;
+  switch (VerifyUser(Username, Code)) {
+    case -1:
+      res.status(404).json({
+        Error: "User not found."
+      });
+      break;
+    case 0:
+      res.status(400).json({
+        Error: "Invalid code or user has already been verified."
+      });
+      break;
+    case 1:
+      res.json({
+        Message: "User successfully verified."
+      });
+      break;
+  }
+});
+
 
 const users = {
     SUKHJIT: {
